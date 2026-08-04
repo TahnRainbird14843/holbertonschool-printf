39790ba2fea682a7a6104d12f277367a596b6d6b
14835-create <14835@holbertonstudents.com>
2026-08-04 02:24:56 +0000

    Update to var_print.c

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdarg.h>
+
 typdef struct print_func
 {
 	char *str;
@@ -14,5 +17,8 @@ int print_str(va_list);
 int print_uint(va_list);
 int (*f)(va_list) get_print_func(char *str);
 int print_string(char *str);
+int count_digits(unsigned int n);
+void print_number(unsigned int n);
+
 
 #endif
